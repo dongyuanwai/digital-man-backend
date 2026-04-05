ad3041664ba8d1ed5fad7e1a4fbeb0dd2425503a
dongyuanwai <yuanwaidong@gmail.com>
2026-04-05 16:29:41 +0800

    feat:vercel sdk使用

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AiModule } from './ai/ai.module';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MailerModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        transport: {
+          host: configService.get('MAIL_HOST'),
+          port: configService.get('MAIL_PORT'),
+          secure: configService.get('MAIL_SECURE') === 'true',
+          auth: {
+            user: configService.get('MAIL_USER'),
+            pass: configService.get('MAIL_PASS'),
+          },
+        },
+        defaults: {
+          from: configService.get('MAIL_FROM'),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+    AiModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
